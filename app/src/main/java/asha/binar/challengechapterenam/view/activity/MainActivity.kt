775dd5aa44f2e12ab973748aa78fe2c026c4e3e4
Asha Imaliaz <asha.imaliazahra2006@students.unila.ac.id>
2022-11-01 09:32:11 +0700

    initial commit

--- app/src/main/java/asha/binar/challengechapterenam/view/activity/MainActivity.kt
+++ app/src/main/java/asha/binar/challengechapterenam/view/activity/MainActivity.kt
@@ -3,12 +3,20 @@ package asha.binar.challengechapterenam.view.activity
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import asha.binar.challengechapterenam.R
+import com.google.firebase.analytics.FirebaseAnalytics
+import com.google.firebase.analytics.ktx.analytics
+import com.google.firebase.ktx.Firebase
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var analytics: FirebaseAnalytics
+
     override fun onCreate(savedInstanceState: Bundle?) {
+        analytics = Firebase.analytics
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
     }
+
 }
